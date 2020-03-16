c11ec0e353ffbb778b9533be50cf74dd138b06dc
limelier <tudor.iacobescu.1@gmail.com>
2020-03-16 16:27:09 +0200

    Initial implementation of Catalog.

--- src/test/java/catalog/Catalog.java
+++ src/test/java/catalog/Catalog.java
@@ -1,4 +1,27 @@
 package catalog;
 
-public class Catalog {
+import document.Document;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Catalog implements Serializable {
+    private String name;
+    private String path;
+    private List<Document> documents;
+
+    public Catalog(String name, String path) {
+        this.name = name;
+        this.path = path;
+        documents = new ArrayList<>();
+    }
+
+    public void add(Document doc) {
+        documents.add(doc);
+    }
+
+    public Document findById(String id) {
+        return documents.stream().filter(doc -> doc.getId().equals(id)).findFirst().orElse(null);
+    }
 }
