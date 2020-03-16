d795d030415b6462815f47f58094f4ed98fe35d4
limelier <tudor.iacobescu.1@gmail.com>
2020-03-16 16:54:29 +0200

    Added CatalogUtil

---
+++ src/test/java/util/CatalogUtil.java
@@ -0,0 +1,38 @@
+package util;
+
+import catalog.Catalog;
+import document.Document;
+
+import java.awt.*;
+import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class CatalogUtil {
+    public static void save(Catalog catalog) throws IOException {
+        try (
+                ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(catalog.getPath()));
+        ) {
+            oos.writeObject(catalog);
+        }
+    }
+
+    public static Catalog load(String path) throws IOException, ClassNotFoundException {
+        try (
+                ObjectInputStream ois = new ObjectInputStream(new FileInputStream(path));
+        ) {
+            return (Catalog) ois.readObject();
+        }
+    }
+
+    public static void view(Document doc) throws IOException, URISyntaxException {
+        Desktop desktop = Desktop.getDesktop();
+        boolean local = doc.getLocation().matches("[a-z]*://.*");
+
+        if (local) {
+            desktop.open(new File(doc.getLocation()));
+        } else {
+            desktop.browse(new URI(doc.getLocation()));
+        }
+    }
+}
