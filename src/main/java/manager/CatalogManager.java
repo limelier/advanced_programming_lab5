840f738813feb25b019125b144be9cc5ffb472bd
limelier <tudor.iacobescu.1@gmail.com>
2020-03-17 13:50:52 +0200

    Started work on the CatalogManager.

---
+++ src/main/java/manager/CatalogManager.java
@@ -0,0 +1,15 @@
+package manager;
+
+import catalog.Catalog;
+import shell.Shell;
+
+public class CatalogManager {
+    Catalog catalog;
+    Shell shell;
+
+    public CatalogManager() {
+        this.shell = new Shell();
+    }
+
+
+}
